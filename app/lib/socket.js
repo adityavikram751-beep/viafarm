7a34e169f80728f7cac27f6abd7e09bffab280d4
TumharaGitHubUsername <TumharaGitHubEmail>
2025-11-08 17:15:13 +0530

    nm

--- app/lib/socket.js
+++ app/lib/socket.js
@@ -1,8 +1,7 @@
 import { io } from "socket.io-client";
 
-const socket = io(process.env.NEXT_PUBLIC_SOCKET_URL || "https://viafarm-1.onrender.com", {
+const socket = io("https://viafarm-1.onrender.com", {
   transports: ["websocket"],
-  withCredentials: true,
 });
 
 export default socket;
